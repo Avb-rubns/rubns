d97ae5f38c9435456f9034ba698d6ef5f0a6f42f
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-07-22 19:07:21 -0500

    Feat: implementacion componentes styled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import '../styles/home.css'
 import Template from '../components/Template'
 import Skillbox from 'components/Container-Skill'
+import ContainerLading from 'components/Container-Lading'
+import ContainerPage from 'components/Container-Home'
+import LoaderSkills from 'components/LoaderSkill'
 import {
   FiArrowDownCircle,
   FiMail,
@@ -25,103 +27,66 @@ export default function Home () {
   return (
     <>
       <Template>
-        <main>
-          <section className="landing">
-            <img src="/images/Logo-light.svg" className="logo" alt="logo" />
-            <h1>Rubén Sánchez Rugerio</h1>
-            <h2 style={{ marginTop: '-0.5rem' }}>Desarrollador de Software</h2>
-            <div className="social-items">
-              <a
-                className="box-social"
-                href="http://"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FiMail />
-              </a>
-              <a
-                className="box-social"
-                href="http://"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FiGithub />
-              </a>
-              <a
-                className="box-social"
-                href="http://"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FiLinkedin />
-              </a>
-            </div>
-            <a className="arrow-down" href="#about-me">
-              <FiArrowDownCircle />
-            </a>
-          </section>
-        </main>
-        <section id="about-me" className="about-me">
-          <h3>Acerca de mí</h3>
-          <div className="about-me-info">
-            <img src="/images/rubns.svg" className="rubns" alt="logo" />
-            <div className="info">
-              <div className="biblograph">
-                <h3>Soy</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Doloremque corrupti officia qui enim nisi dolor magnam quod
-                  dicta eligendi, ea repellendus ipsa excepturi vel delectus
-                  odio iste nobis quaerat voluptas.
-                </p>
-              </div>
-              <div className="education">
-                <h3>Educación</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Doloremque corrupti officia qui enim nisi dolor magnam quod
-                  dicta eligendi, ea repellendus ipsa excepturi vel delectus
-                  odio iste nobis quaerat voluptas.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section id="skills" className="Skills">
-          <h3>Conocimientos</h3>
-          <div className="grid-skills">
-          <Skillbox name="HTML5" >
-            <SiHtml5/>
-          </Skillbox>
-          <Skillbox name="CSS3" >
-              <SiCss3 />
-          </Skillbox>
-            <Skillbox name="JavaScript">
-              <SiJavascript />
+        <ContainerPage
+         about="Acerca de mí"
+         im="Soy"
+
+         biograph ="  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Doloremque corrupti officia qui enim nisi dolor magnam quod
+         dicta eligendi, ea repellendus ipsa excepturi vel delectus
+         odio iste nobis quaerat voluptas."
+         education=" Educación"
+
+         infoEdu="Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+         Doloremque corrupti officia qui enim nisi dolor magnam quod
+         dicta eligendi, ea repellendus ipsa excepturi vel delectus
+         odio iste nobis quaerat voluptas."
+         skills="Conocimientos"
+         >
+          <ContainerLading
+          name="Rubén Sánchez Rugerio"
+          occupation ="Desarrollador de Software"
+          id = "lading"
+          link= "https://github.com/Avb-rubns"
+           >
+             <FiMail />
+             <FiGithub />
+             <FiLinkedin />
+             <FiArrowDownCircle />
+          </ContainerLading>
+          <LoaderSkills>
+            <Skillbox name="HTML5" >
+              <SiHtml5/>
             </Skillbox>
-            <Skillbox name="Git">
-              <SiGit />
+            <Skillbox name="CSS3" >
+                <SiCss3 />
             </Skillbox>
-            <Skillbox name="Gnu/Bash">
-              <SiGnubash />
-            </Skillbox>
-            <Skillbox name="Java">
-              <SiJava />
-            </Skillbox>
-            <Skillbox name="Python">
-              <SiPython />
-            </Skillbox>
-            <Skillbox name="MS SQL Server">
-              <SiMicrosoftsqlserver/>
-            </Skillbox>
-            <Skillbox name="MySQL">
-              <SiMysql />
-            </Skillbox>
-            <Skillbox name="MongoDB">
-              <SiMongodb />
-            </Skillbox>
-          </div>
-        </section>
+              <Skillbox name="JavaScript">
+                <SiJavascript />
+              </Skillbox>
+              <Skillbox name="Git">
+                <SiGit />
+              </Skillbox>
+              <Skillbox name="Gnu/Bash">
+                <SiGnubash />
+              </Skillbox>
+              <Skillbox name="Java">
+                <SiJava />
+              </Skillbox>
+              <Skillbox name="Python">
+                <SiPython />
+              </Skillbox>
+              <Skillbox name="MS SQL Server">
+                <SiMicrosoftsqlserver/>
+              </Skillbox>
+              <Skillbox name="MySQL">
+                <SiMysql />
+              </Skillbox>
+              <Skillbox name="MongoDB">
+                <SiMongodb />
+              </Skillbox>
+          </LoaderSkills>
+        </ContainerPage>
       </Template>
     </>
   )
