a1cae65f24f79c1dab4d0fe50a0e5d7488584a42
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-07-22 19:05:27 -0500

    Feat: body the social items and arrow

---
+++ src/components/Container-Lading/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Box, Arrow } from './container.styles'
+import LoaderIcon from 'components/LoaderIcon'
+
+export default function ContainerLading (props) {
+  return (
+        <Container id = {props.id}>
+            <h1>{props.name}</h1>
+            <h2>{props.occupation}</h2>
+            <div>
+                <Box href={props.mail} target="_blank" rel="noopener noreferrer" >
+                 <LoaderIcon >{props.children[0] }</LoaderIcon>
+                </Box>
+                <Box href={props.git} target="_blank" rel="noopener noreferrer" >
+                <LoaderIcon >{props.children[1] }</LoaderIcon>
+                </Box>
+                <Box href={props.link} target="_blank" rel="noopener noreferrer" >
+                <LoaderIcon >{props.children[2] }</LoaderIcon>
+                </Box>
+            </div>
+            <Arrow href="#about-me">
+            {props.children[3] }
+            </Arrow>
+        </Container>
+  )
+}
