756817b3201c96bce5a27ad1eda715fac1fb8961
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-07-30 14:34:27 -0500

    Test

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/globals.styles'
 import Router from './router/Router'
-import light from './styles/schemes/light'
+import Dark from './styles/schemes/dark'
+// import light from 'styles/schemes/light'
 
 ReactDOM.render(
-  <ThemeProvider theme={light}>
+  <ThemeProvider theme={Dark}>
     <GlobalStyle />
     <Router />
   </ThemeProvider>,
