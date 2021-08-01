a3e3cf0e9e920c3ea8198803e705ef26e435289a
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-07-31 20:25:31 -0500

    Test: create card vanilla and styles

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -22,7 +22,70 @@ export default function Notes () {
         </main>
         <section>
           <h3>Resultados de la búsqueda</h3>
-          <div></div>
+          <div className="grid">
+            <div className="card">
+              <h3>Instalacion de PyQt5 desde CLI</h3>
+              <p>En esta nota muestro como instalar y configurar el paquete de pyqt (creación de interfaces)
+              </p>
+              <div className="box-tags">
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+                <span className="tag" >CLI</span>
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+                <span className="tag" >CLI</span>
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+                <span className="tag" >CLI</span>
+              </div>
+              <button>Leer Nota</button>
+            </div>
+            <div className="card">
+            <h3>Instalacion de PyQt5 desde CLI</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Necessitatibus,
+                a quod. Atque, aliquid consectetur,
+                autem quas vero ducimus maiores temporibus ab,
+                ratione nostrum nobis quidem?
+              </p>
+              <div >
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+              </div>
+              <button>Leer Nota</button>
+            </div>
+            <div className="card">
+              <h3>Instalacion de PyQt5 desde CLI</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Necessitatibus,
+                a quod. Atque, aliquid consectetur,
+                autem quas vero ducimus maiores temporibus ab,
+                ratione nostrum nobis quidem?
+              </p>
+              <div>
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+              </div>
+              <button>Leer Nota</button>
+            </div>
+            <div className="card">
+              <h3>Instalacion de PyQt5 desde ClL</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Necessitatibus,
+                a quod. Atque, aliquid consectetur,
+                autem quas vero ducimus maiores temporibus ab,
+                ratione nostrum nobis quidem?
+              </p>
+              <div>
+                <span className="tag">Python</span>
+                <span className="tag" >Programacion</span>
+                <span className="tag" >CLI</span>
+              </div>
+              <button>Leer Nota</button>
+            </div>
+          </div>
         </section>
       </Template>
     </>
