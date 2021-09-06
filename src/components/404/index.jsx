6204185ae998adcde83dec42687f55896b618a95
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-06 18:23:12 -0500

    Feat: Creat layout page 404

---
+++ src/components/404/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Card, Container, Rectangle } from './container.styles'
+
+export default function NotFound (props) {
+  return (
+    <Container>
+        <div>
+            <article>
+                <h1>&lt;/&gt;</h1>
+                <section>
+                    <Rectangle background="--black-light" size="50"/>
+                    <Rectangle background="--main-color" size="150" />
+                    <Rectangle background="--gray-dark" size="350"/>
+                    <Rectangle background="--white" />
+                    <Rectangle background="--black-light" size="450"/>
+                    <Rectangle background="--white" size="50" />
+                    <Rectangle background="--gray"/>
+                </section>
+            </article>
+
+            <section>
+                <Rectangle background="--black-light" size="50"/>
+
+                <Rectangle background="--gray-dark" size="250"/>
+                <Rectangle background="--main-color" size="50" />
+                <Rectangle background="--white" />
+                <Rectangle background="--black-light" size="450"/>
+                <Rectangle background="--white" size="50" />
+                <Rectangle background="--gray"/>
+
+                <Rectangle background="--gray-dark" />
+                <Rectangle background="--gray" size="450"/>
+                <Rectangle background="--white" size="50" />
+                <Rectangle background="--black-light"/>
+            </section>
+
+            <section>
+                <Card><h1>¡UPS!</h1></Card>
+                <Card><h1>404</h1></Card>
+                <Card><h1>😅</h1></Card>
+            </section>
+        </div>
+        <h1>{props.text}</h1>
+    </Container>
+  )
+}
