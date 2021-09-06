b79bd423be97d92262eb6db3d6c9669f2fc2129d
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-06 18:26:36 -0500

    Fix: render fonud

--- src/styles/globals.styles.js
+++ src/styles/globals.styles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 import { colors } from './colors.styles'
 import { typography } from './fonts.styles'
 import NotoSansSC from '../assets/fonts/NotoSansSC-Regular.otf'
-
+import NotoSansSCBold from '../assets/fonts/NotoSansSC-Bold.otf'
 export const GlobalStyle = createGlobalStyle`
   /* Colors variables */
   ${colors}
@@ -13,6 +13,12 @@ export const GlobalStyle = createGlobalStyle`
  @font-face{
      font-family: "NotoSans SC Regular";
      src: url(${NotoSansSC});
+     font-display: fallback;
+ }
+ @font-face{
+     font-family: "NotoSans SC Bold";
+     src: url(${NotoSansSCBold});
+     font-display: fallback;
  }
 
   /* General styles */
@@ -25,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family: 'NotoSans SC Regular', system-ui, sans-serif;
+    font-family: 'NotoSans SC Regular','NotoSans SC Bold', system-ui, sans-serif;
     background: ${({ theme }) => theme.colors.body.background};
     color: ${({ theme }) => theme.colors.body.text};
     scroll-behavior: smooth;
@@ -47,7 +53,7 @@ export const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     margin: 0;
-    font-family: 'NotoSans SC Regular', system-ui, sans-serif;
+    font-family: 'NotoSans SC Regular', 'NotoSans SC Bold', system-ui, sans-serif;
   }
   h1 {
     font-size: var(--h1);
