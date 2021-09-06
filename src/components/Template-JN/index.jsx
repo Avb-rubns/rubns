3dd806ab102b8fd0304b480af09752495be9a898
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-06 18:25:23 -0500

    test: information

--- src/components/Template-JN/index.jsx
+++ src/components/Template-JN/index.jsx
@@ -4,21 +4,51 @@ import Btn from 'components/button'
 import { RenderCard } from 'components/RenderCard'
 
 export default function TemplateJN (props) {
+  const res = {
+    data: [
+      {
+        title: 'Instalacion de PyQt5 desde CLI',
+        description: 'En esta nota muestro como instalar y configurar el paquete de pyqt (creación de interfaces)',
+        tags: ['Python', 'PYQT5', 'Programación', 'CLI'],
+        code: 'https://github.com/davidp46/todo-app-client/blob/main/src/components/button/button.styles.js'
+      },
+      {
+        title: 'Instalacion de PyQt5 desde CLI',
+        description: 'En esta nota muestro como instalar y configurar el paquete de pyqt (creación de interfaces)',
+        tags: ['Python', 'PYQT5', 'Programación'],
+        code: 'https://github.com/davidp46/todo-app-client/blob/main/src/components/button/button.styles.js'
+      },
+      {
+        title: 'Instalacion de PyQt5 desde CLI',
+        description: 'En esta nota muestro como instalar y configurar el paquete de pyqt (creación de interfaces)',
+        tags: ['Python', 'PYQT5', 'Programación', 'CLI'],
+        code: 'https://github.com/davidp46/todo-app-client/blob/main/src/components/button/button.styles.js',
+        link: 'rubns.me'
+      },
+      {
+        title: 'Instalacion de PyQt5 desde CLI',
+        description: 'En esta nota muestro como instalar y configurar el paquete de pyqt (creación de interfaces)',
+        tags: ['Python', 'Programación'],
+        code: 'https://github.com/davidp46/todo-app-client/blob/main/src/components/button/button.styles.js',
+        link: 'rubns.me'
+      }
+    ]
+  }
   return (
         <>
         <Header>
             <h1>{props.title}</h1>
             <div>
                 <Input type={props.type} placeholder={props.placeholder} />
-                <Btn text={props.textBtn}>{props.children}</Btn>
+                <Btn type="primary" size="16" text={props.textBtn}> {props.children} </Btn>
             </div>
         </Header>
-        <Finds>
+        {res && <Finds>
           <h3>{props.search}</h3>
           <div>
-            <RenderCard result={props.result.res} />
+            <RenderCard result={res} />
           </div>
-        </Finds>
+        </Finds>}
         </>
   )
 }
