d14f7acf2ba6d819e20b31149b18883902a1c6fb
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-06 18:25:01 -0500

    Fix: button test and rendering

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -5,12 +5,15 @@ import { RenderTag } from 'components/RenderTag'
 export default function CardInfo (props) {
   return (
         <Card>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-        <div>
-            <RenderTag tags={props.tags}/>
-        </div>
-        <Btn text="Leer" />
+          <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <div>
+              <RenderTag tags={props.tags}/>
+          </div>
+          <div>
+            {props.link && <Btn size="14" link={props.link} text="Ver proyecto"/>}
+            {props.code && <Btn size="14" code={props.code} text="Ver código"/>}
+          </div>
     </Card>
   )
 }
