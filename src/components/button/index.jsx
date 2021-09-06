b9072da78799de7bbd5a277571192bdbd89de265
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-06 18:24:26 -0500

    test: function click

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,10 +2,22 @@ import LoaderIcon from 'components/LoaderIcon'
 import { Button } from './button.styled'
 
 export default function Btn (props) {
+  const view = (info) => {
+    try {
+      if (info.link) {
+        console.log(info.link)
+      } else if (info.code) {
+        console.log(info.code)
+      }
+    } catch (error) {
+      console.error()
+    }
+  }
   return (
-        <Button>
+        <Button type={props.type} size={props.size} onClick={() => view({ link: props.link, code: props.code })}
+          >
             <LoaderIcon color="var(--main-color)" >{props.children}</LoaderIcon>
-            <p>{props.text}</p>
+            <p>{props.text ? props.text : 'Buscar'}</p>
         </Button>
   )
 }
