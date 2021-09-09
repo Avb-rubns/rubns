ca506e1d6d565fb49d7cbcf41fa4f2585b345e61
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-08 19:28:20 -0500

    Fix: method in props

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,19 +2,8 @@ import LoaderIcon from 'components/LoaderIcon'
 import { Button } from './button.styled'
 
 export default function Btn (props) {
-  const view = (info) => {
-    try {
-      if (info.link) {
-        console.log(info.link)
-      } else if (info.code) {
-        console.log(info.code)
-      }
-    } catch (error) {
-      console.error()
-    }
-  }
   return (
-        <Button type={props.type} size={props.size} onClick={() => view({ link: props.link, code: props.code })}
+        <Button type={props.type} size={props.size} onClick={props.method}
           >
             <LoaderIcon >{props.children}</LoaderIcon>
             <p>{props.text ? props.text : ''}</p>
