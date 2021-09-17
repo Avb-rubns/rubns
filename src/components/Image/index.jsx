7d37715a17668be14976759b346ef3dcccbeb039
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-16 19:03:46 -0500

    Feat: create componet Image and styles

---
+++ src/components/Image/index.jsx
@@ -0,0 +1,7 @@
+import { Img } from './styled.img'
+
+export default function Image (props) {
+  return (
+        <Img src={props.url}/>
+  )
+}
---
+++ src/components/Image/styled.img.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Img = styled.img`
+border-radius: 50%;
+box-shadow: 0px 0px 16px;
+border: 1px;
+color: ${({ theme }) => theme.colors.body.text};
+
+`
