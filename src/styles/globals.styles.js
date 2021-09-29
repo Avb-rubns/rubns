97cf283622b29bf328bd62c39d65333a6a9df9aa
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-29 14:14:43 -0500

    Delete loafin fonts local

--- src/styles/globals.styles.js
+++ src/styles/globals.styles.js
@@ -1,31 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
 import { colors } from './colors.styles'
 import { typography } from './fonts.styles'
-import NotoSansSC from '../assets/fonts/NotoSansSC-Regular.otf'
-import NotoSansSCBold from '../assets/fonts/NotoSansSC-Bold.otf'
-import NotoSansSCLight from '../assets/fonts/NotoSansSC-Light.otf'
 export const GlobalStyle = createGlobalStyle`
   /* Colors variables */
   ${colors}
 
   /* Fonts variables */
   ${typography}
-  /*font-facer*/
- @font-face{
-     font-family: "NotoSans SC Regular";
-     src: url(${NotoSansSC});
-     font-display: fallback;
- }
- @font-face{
-     font-family: "NotoSans SC Bold";
-     src: url(${NotoSansSCBold});
-     font-display: fallback;
- }
- @font-face{
-     font-family: "NotoSans SC Light";
-     src: url(${NotoSansSCLight});
-     font-display: fallback;
- }
 
   /* General styles */
   :root {
@@ -37,7 +18,7 @@ export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    font-family: 'NotoSans SC Regular','NotoSans SC Bold','NotoSans SC Light', system-ui, sans-serif;
+    font-family: 'Noto Sans Display' system-ui, sans-serif;
     background: ${({ theme }) => theme.colors.body.background};
     color: ${({ theme }) => theme.colors.body.text};
     scroll-behavior: smooth;
@@ -48,6 +29,7 @@ export const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     display: inline-flex;
     cursor: pointer;
+    font-weight: var(--medium);
   }
 
   a:hover {
@@ -59,11 +41,10 @@ export const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     margin: 0;
-    font-family: 'NotoSans SC Regular', 'NotoSans SC Bold', 'NotoSans SC Light', system-ui, sans-serif;
+    font-family: 'Noto Sans Display', system-ui, sans-serif;
   }
   h1 {
     font-size: var(--h1);
-    font-family: 'NotoSans SC Bold';
   }
   h2 {
     font-size: var(--h2);
@@ -72,12 +53,16 @@ export const GlobalStyle = createGlobalStyle`
     font-size: var(--h3);
   }
 
+  p{
+    font-size: var(--p);
+  }
+
   textarea,
   button,
   input {
     border: 0;
     outline: 0;
-    font-family: 'NotoSans SC Regular', system-ui, sans-serif;
+    font-family: 'Noto Sans Display', system-ui, sans-serif;
   }
   button {
     cursor: pointer;
