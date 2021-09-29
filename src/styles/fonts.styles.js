54508622e9870bffa4698be448ae0c5fc9044413
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-29 14:15:30 -0500

    add: weight fonts

--- src/styles/fonts.styles.js
+++ src/styles/fonts.styles.js
@@ -6,5 +6,17 @@ export const typography = css`
     --h1: 36px;
     --h2: 24px;
     --h3: 20px;
+    --p: 14px;
+    --caption: 12px;
   }
+
+  /* font weights */
+  :root {
+    --bold: 700;
+    --semi: 600;
+    --medium: 500;
+    --regular: 400;
+    --light: 300;
+  }
+
 `
