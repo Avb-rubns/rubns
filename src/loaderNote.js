abd31ad9ca0729c1aff8c9212e31785b1ba522b6
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-10-01 14:18:08 -0500

    Create: loader note and note

---
+++ src/loaderNote.js
@@ -0,0 +1,7 @@
+import Content from '!babel-loader!@mdx-js/loader!./conten/notes/Instalacion-de-PYQT-5.mdx'
+
+function App () {
+  return <Content />
+}
+
+export default App
--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -1,9 +1,11 @@
 import Template from 'components/Template'
 import React from 'react'
+import App from 'loaderNote'
 
 export default function Read (props) {
   return (
         <Template>
+          <App/>
         </Template>
   )
 }
